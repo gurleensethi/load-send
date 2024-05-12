4889cd3276f1225b445bf6a603b835f5dadb5406
Gurleen Sethi <sarusethi@rocketmail.com>
2024-05-12 16:37:52 -0400

    feat: add success and failed RPS

--- reporter/http.go
+++ reporter/http.go
@@ -22,13 +22,15 @@ type report struct {
 }
 
 type HttpStatusReporter struct {
-	totalRequests        int
-	totalSuccessRequests int
-	totalFailedRequests  int
-	totalRequestTime     int
-	failedRequestReasons map[string]int
-	reportCh             chan report
-	wg                   *sync.WaitGroup
+	totalRequests           int
+	totalSuccessRequests    int
+	totalFailedRequests     int
+	totalSuccessRequestTime int
+	totalFailedRequestTime  int
+	totalRequestTime        int
+	failedRequestReasons    map[string]int
+	reportCh                chan report
+	wg                      *sync.WaitGroup
 }
 
 func (r *HttpStatusReporter) Start() {
@@ -57,12 +59,14 @@ func (r *HttpStatusReporter) Stop() {
 func (r *HttpStatusReporter) ReportSuccessRequest(duration int) {
 	r.totalRequests++
 	r.totalSuccessRequests++
+	r.totalSuccessRequestTime += duration
 	r.totalRequestTime += duration
 }
 
 func (r *HttpStatusReporter) ReportFailedRequest(duration int, reason string) {
 	r.totalRequests++
 	r.totalFailedRequests++
+	r.totalFailedRequestTime += duration
 	r.totalRequestTime += duration
 	r.failedRequestReasons[reason]++
 }
@@ -75,6 +79,21 @@ func (r *HttpStatusReporter) Print() {
 		averageLatency = r.totalRequestTime / r.totalRequests
 	}
 
+	var successRPS float64
+	if r.totalSuccessRequests > 0 {
+		successRPS = float64(r.totalSuccessRequests) / float64(r.totalSuccessRequestTime/1000)
+	}
+
+	var failedRPS float64
+	if r.totalFailedRequests > 0 {
+		failedRPS = float64(r.totalFailedRequests) / float64(r.totalFailedRequestTime/1000)
+	}
+
+	var totalRPS float64
+	if r.totalRequests > 0 {
+		totalRPS = float64(r.totalRequests) / float64(r.totalRequestTime/1000)
+	}
+
 	fmt.Fprintf(writer, "\n\n")
 	fmt.Fprintf(writer, "Total Requests \t %d\n", r.totalRequests)
 	fmt.Fprintf(writer, "Total Success Requests \t %d\n", r.totalSuccessRequests)
@@ -84,9 +103,13 @@ func (r *HttpStatusReporter) Print() {
 			fmt.Fprintf(writer, "%s \t %d\n", key, value)
 		}
 	}
+	fmt.Fprintf(writer, "Total Success Request Time (ms) \t %d\n", r.totalSuccessRequestTime)
+	fmt.Fprintf(writer, "Total Failed Request Time (ms) \t %d\n", r.totalFailedRequestTime)
 	fmt.Fprintf(writer, "Total Request Time (ms) \t %d\n", r.totalRequestTime)
 	fmt.Fprintf(writer, "Average Latency (ms) \t %d\n", averageLatency)
-	fmt.Fprintf(writer, "requests/sec \t %0.2f\n", float64(r.totalRequests)/float64(r.totalRequestTime/1000))
+	fmt.Fprintf(writer, "Success RPS \t %0.2f\n", successRPS)
+	fmt.Fprintf(writer, "Failed RPS \t %0.2f\n", failedRPS)
+	fmt.Fprintf(writer, "Total RPS \t %0.2f\n", totalRPS)
 	_ = writer.Flush()
 	fmt.Fprintf(writer, "\n\n")
 }
